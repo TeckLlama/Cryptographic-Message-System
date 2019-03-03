5a6332eedcd0e9b2347635db3d9f58f8287c4d5b
Elliot <elliot194@gmail.com>
2019-03-03 00:21:59 +0000

    Copying project
    Added to start orginizing code into separate cpp and header files

---
+++ Cryptographic_Message_System_V3/Cryptographic_Message_System/Cryptographic_Message_System/Time.h
@@ -0,0 +1,18 @@
+// Auther: Elliot Rihan
+// Created: 19/02/19
+// Last Modified Date: 
+// Description: C++ program 
+// Time.h: This is the header file for Time.cpp
+
+#pragma once
+using namespace std;
+namespace N
+{
+	class time
+	{
+	public:
+	
+		void getTimeHH();
+	}
+};
+}
